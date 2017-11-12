2b726a394495695ad23b184835ed260998e43b08
Jeffrey Ng <jeffng6688@gmail.com>
2017-11-11 16:04:28 -0800

    Config file change

--- src/server/config.js
+++
@@ -1,4 +0,0 @@
-const accountSid = 'ACacd1acc379176f9033eba0c86b2f5244';
-const authToken = 'd4a1d6724a96c2ec6801f59d2494c25b';
-module.exports.accountSid = accountSid;
-module.exports.authToken = authToken;
---
+++ src/server/configExample.js
@@ -0,0 +1,4 @@
+const accountSid = 'YOUR ACCOUNTSID';
+const authToken = 'YOUR AUTHTOKEN';
+module.exports.accountSid = accountSid;
+module.exports.authToken = authToken;
